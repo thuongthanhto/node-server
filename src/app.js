3061877b541caca46ff4ece677ce67ef003c5fbb
thuongtt <thuongtt@sendo.vn>
2019-12-30 09:48:23 +0700

    change file struct

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const jsonwebtoken = require('jsonwebtoken');
@@ -57,6 +56,4 @@ app.use(function(req, res) {
   res.status(404).send({ url: req.originalUrl + ' not found' });
 });
 
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
+module.exports = app;
